ef3972a83a0a25f7f4b63b948855ed2767e5f30a
Henrik Nelson <henrik@cliffords.nu>
2017-08-15 15:49:01 +0200

    Added methods to set/get light brightness and name

--- src/main/java/nu/cliffords/kyee/Light.kt
+++ src/main/java/nu/cliffords/kyee/Light.kt
@@ -1,21 +1,16 @@
 package nu.cliffords.kyee
 
 import android.os.Handler
-import android.system.Os
 import android.util.Log
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.android.extension.responseJson
 import com.github.kittinunf.fuel.core.FuelManager
-import android.system.Os.socket
-import android.system.Os.socket
-import java.io.IOException
-import java.lang.reflect.Array.getLength
-import android.system.Os.socket
 import org.json.JSONObject
 import java.net.*
 import android.os.Looper
-
-
+import android.util.Base64
+import nu.cliffords.kyee.teeceepee.TCPClient
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.nio.charset.Charset
 
 
 /**
@@ -23,30 +18,31 @@ import android.os.Looper
  */
 class Light {
 
+    var name: String = ""
+    var uri: URI? = null
     var location: String = ""
     var id: String = ""
     var model: String = ""
     var firmware_version: String = ""
     var support: Array<String> = emptyArray()
     var power: Boolean = false
+    var brightness: Int = 0
 
+    enum class ColorMode(val mode: Int) {
+        COLOR(1),
+        COLOR_TEMP(2),
+        HSV(3)
+    }
 
-    init {
-        //FuelManager.instance.basePath = "http://$ipAddress:55443"
-        FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
-        FuelManager.instance.addRequestInterceptor { request ->
-            Log.d("kYee",request.toString())
-            request
-        }
-        FuelManager.instance.addResponseInterceptor { response ->
-            Log.d("kYee",response.toString())
-            response
-        }
+    enum class LightEffect(val value: String)
+    {
+        SUDDEN("sudden"),
+        SMOOTH("smooth")
     }
 
     companion object Factory {
 
-        fun getLights(listener: (List<Light>) -> Unit, timeout: Int = 4) {
+        fun getLights(listener: (List<Light>) -> Unit, timeout: Int = 2) {
             val socket = MulticastSocket() // must bind receive side
             sendDiscovery(socket)
             var lightList: MutableList<Light> = mutableListOf()
@@ -54,15 +50,18 @@ class Light {
                 lightMap.forEach { _,value ->
                     Log.d("kYee",value.toString())
                     val light = Light()
+                    light.name = Base64.decode(value.getString("name"),Base64.DEFAULT).toString(Charset.forName("UTF-8"))
                     light.id = value.getString("id")
-                    light.location = value.getString("Location").split("yeelight://")[1]
-                    light.firmware_version = value.getString("fw_ver")
+                    light.location = value.getString("Location")
+                    light.uri = URI.create(light.location)
+                    light.firmware_version =  value.getString("fw_ver")
                     light.model = value.getString("model")
                     light.support = value.getString("support").split(" ").toTypedArray()
                     light.power = value.getString("power") == "on"
+                    light.brightness = value.getString("bright").toInt()
                     lightList.add(light)
                 }
-                Handler(Looper.getMainLooper()).post(Runnable {listener(lightList)  })
+                listener(lightList)
             },timeout)
 
         }
@@ -80,8 +79,8 @@ class Light {
         }
 
         //Try to receive multicast responses
-        private fun receiveDiscoveredDevices(socket:MulticastSocket, listener: (Map<String,JSONObject>) -> Unit, timeout: Int = 4 ) {
-            Thread(Runnable {
+        private fun receiveDiscoveredDevices(socket:MulticastSocket, listener: (Map<String,JSONObject>) -> Unit, timeout: Int = 2 ) {
+            doAsync {
                 val returnMap: MutableMap<String,JSONObject> = mutableMapOf()
                 val startTime = System.currentTimeMillis()
                 socket.soTimeout = 1000
@@ -114,22 +113,75 @@ class Light {
                     }
 
                 }
-                listener(returnMap)
-            }).start()
+                uiThread {
+                    listener(returnMap)
+                }
+
+            }
         }
 
     }
 
-    fun setPower(on: Boolean, listener: (String)->Unit){
-        var state = ""
-        if(on)
-            state = "on"
-        else
-            state = "off"
-        Fuel.post("http://$location").body("{ \"id\": 1, \"method\": \"set_power\", \"params\":[\"on\", \"smooth\", 500]}").responseJson { request, response, result ->
-            val (json,error) = result
-            if(json != null)
-                listener(json.toString())
-        }
+
+    private fun getProp(propName: String, listener: (String) -> Unit) {
+        TCPClient.send(this.uri!!,"get_prop",arrayListOf<String>(propName),
+                { jsonResponse ->
+                    listener(jsonResponse.getJSONArray("result").getString(0))
+                },{ errorMessage ->
+            Log.e("kYee","Could not get property - reason: $errorMessage")
+        })
+    }
+
+    fun setPower(state: Boolean, effect: LightEffect, duration:Int, listener: (JSONObject) -> Unit) {
+        var stateString = if (state) "on" else "off"
+        val params = arrayListOf<Any>(stateString,effect.value,duration)
+        TCPClient.send(this.uri!!,"set_power",params,
+        { jsonResponse ->
+            this.power = state
+            listener(jsonResponse)
+        },{ errorMessage ->
+            Log.e("kYee","Could not set power - reason: $errorMessage")
+        })
+    }
+
+    fun getPower(listener: (Boolean) -> Unit){
+        this.getProp("power",{ stringValue ->
+            listener(stringValue.equals("on"))
+        })
+    }
+
+    fun setBrightness(brightness: Int, effect: LightEffect, duration:Int, listener: (JSONObject) -> Unit) {
+        val params = arrayListOf<Any>(if(brightness <= 0) 1 else if(brightness > 100) 100 else brightness,effect.value,duration)
+        TCPClient.send(this.uri!!,"set_bright",params,
+                { jsonResponse ->
+                    this.brightness = brightness
+                    listener(jsonResponse)
+                },{ errorMessage ->
+            Log.e("kYee","Could not set brightness - reason: $errorMessage")
+        })
+    }
+
+    fun getBrightness(listener: (Int) -> Unit){
+        this.getProp("bright",{ stringValue ->
+            listener(stringValue.toInt())
+        })
+    }
+
+    fun setName(name: String, listener: (JSONObject) -> Unit) {
+        //val encodedName = Base64.encodeToString(name.toByteArray(Charset.forName("UTF-8")),Base64.DEFAULT);
+        val params = arrayListOf<Any>(name)
+        TCPClient.send(this.uri!!,"set_name",params,
+                { jsonResponse ->
+                    this.name = name
+                    listener(jsonResponse)
+                },{ errorMessage ->
+            Log.e("kYee","Could not set name - reason: $errorMessage")
+        })
+    }
+
+    fun getName(listener: (String) -> Unit){
+        this.getProp("name",{ stringValue ->
+            listener(stringValue)
+        })
     }
 }
